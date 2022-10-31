6d5f2ecc3d862523eecafbce0a5d33d2e8896c05
akcy01 <98681852+akcy01@users.noreply.github.com>
2022-10-31 19:13:44 +0300

    Add project files.

---
+++ SultanPilic/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SultanPilic.Data;
+using SultanPilic.Models;
+
+namespace SultanPilic.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _dbContext = context;   
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList= _dbContext.Categories;
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            return View();
+        }
+    }
+}
---
+++ SultanPilic/Migrations/20221030172951_CategoryUpdated.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SultanPilic.Migrations
+{
+    public partial class CategoryUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
