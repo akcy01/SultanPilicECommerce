f3b2649326ac8b96c4f1477a55af29f958d61fb9
akcy01 <oakcy01@icloud.com>
2022-11-26 15:25:54 +0300

    database renewed.

---
+++ Sultan.DataAccess/Migrations/20221126120726_MigrateUpdate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sultan.DataAccess.Migrations
+{
+    public partial class MigrateUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
