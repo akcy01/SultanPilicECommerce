86e572e7f4b5d5a0c579df299bb49f76005d40a6
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2022-12-03 17:51:01 +0300

    Edit and Delete button added.

--- SultanPilic/wwwroot/js/product.js
+++ SultanPilic/wwwroot/js/product.js
@@ -14,6 +14,20 @@ function loadDataTable() {
             { "data": "description", "width": "15%" },
             { "data": "price", "width": "15%" },
             { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                   <div class="w-75 btn-group" role="group">
+                    <a href="/Admin/Product/Upsert?id=${data}" 
+                    class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Düzenle</a>
+                    <a  
+                    class="btn btn-danger mx-2"> <i class="bi bi-trash3"></i> Sil</a>
+                   </div>
+                    `
+                },
+                "width" : "15%"
+            }
         ]
     });
 }
