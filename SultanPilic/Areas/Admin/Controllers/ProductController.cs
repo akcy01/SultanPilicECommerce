f437f721b9eda6a05b32de6a9583881082b86385
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2022-12-04 16:08:48 +0300

    Delete completed

--- SultanPilic/Areas/Admin/Controllers/ProductController.cs
+++ SultanPilic/Areas/Admin/Controllers/ProductController.cs
@@ -14,9 +14,9 @@ namespace SultanPilic.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _hostEnvironment;
-        public ProductController(IUnitOfWork unitOfWork,IWebHostEnvironment hostEnvironment)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
-            _unitOfWork = unitOfWork;  
+            _unitOfWork = unitOfWork;
             _hostEnvironment = hostEnvironment;
         }
         public IActionResult Index()
@@ -35,15 +35,15 @@ namespace SultanPilic.Controllers
                 }),
                 ChickTypeList = _unitOfWork.ChickType.GetAll().Select(i => new SelectListItem
                 {
-                    Text=i.Name,
-                    Value=i.Id.ToString()
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 }),
             };
 
             if (id == null || id == 0)
             {
-            //    ViewBag.CategoryList = CategoryList;
-            //    ViewData["ChickTypeList"] = ChickTypeList;
+                //    ViewBag.CategoryList = CategoryList;
+                //    ViewData["ChickTypeList"] = ChickTypeList;
                 return View(productVM);
             }
             else
@@ -54,20 +54,20 @@ namespace SultanPilic.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(ProductVM obj , IFormFile file) //IFormFile dosya yüklememizi sağlar.
+        public IActionResult Upsert(ProductVM obj, IFormFile file) //IFormFile dosya yüklememizi sağlar.
         {
             if (ModelState.IsValid)
             {
                 string wwwRootPath = _hostEnvironment.WebRootPath;
-                if(file != null)
+                if (file != null)
                 {
                     string fileName = Guid.NewGuid().ToString();//Benzersiz bir dosya adı oluşturmak için bunu yaptık.
                     var uploads = Path.Combine(wwwRootPath, @"images\products");//Yüklenen görselin gideceği klasörü belirttik.
                     var extension = Path.GetExtension(file.FileName);//Uzantıları test ettik burada.
 
-                    if(obj.Product.ImageUrl!= null)
+                    if (obj.Product.ImageUrl != null)
                     {
-                        var oldImagePath = Path.Combine(wwwRootPath,obj.Product.ImageUrl.TrimStart('\\'));
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
                         if (System.IO.File.Exists(oldImagePath))
                         {
                             System.IO.File.Delete(oldImagePath); //Görseli güncelleyebilmek için öncelikle varolanı silmeliyiz.Burada o işlemi gerçekleştirdim.
@@ -95,40 +95,34 @@ namespace SultanPilic.Controllers
             }
             return View(obj);
         }
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var categoryFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
-            if (categoryFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(categoryFromDb);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePost(int? id)
-        {
-            var obj = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Category.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Kategori başarıyla silindi";
-            return RedirectToAction("Index");
-        }
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
-            var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,ChickType");
-            return Json(new {data = productList});
+            var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ChickType");
+            return Json(new { data = productList });
         }
-        #endregion
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Silinirken hata oluştu" });
+            }
+
+            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Başarıyla silindi" });
+        }
     }
+    #endregion
 }
+
