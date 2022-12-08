6153e86303cfea82e886cf473d8e87b2ad6808e0
Oğuz Akçay <98681852+akcy01@users.noreply.github.com>
2022-12-08 19:40:55 +0300

    DetailsAction added.

--- SultanPilic/Areas/Customer/Controllers/HomeController.cs
+++ SultanPilic/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sultan.DataAccess.Repository.IRepository;
 using Sultan.Models;
+using Sultan.Models.ViewModels;
 using System.Diagnostics;
+using System.Security.Policy;
 
 namespace SultanPilic.Controllers
 {
@@ -15,18 +17,24 @@ namespace SultanPilic.Controllers
             _logger = logger;
             _unitOfWork = unitOfWork;
         }
-
         public IActionResult Index()
         {
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ChickType");
             return View(productList);
         }
-
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                count = 1,
+                product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,ChickType")
+            };
+            return View(cartObj);
+        }
         public IActionResult Privacy()
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
